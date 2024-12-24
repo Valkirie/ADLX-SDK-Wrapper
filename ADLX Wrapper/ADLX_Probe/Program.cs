176cf611625f8a0abb9e311a7d7fa4e9df1a2637
Lesueur Benjamin <benjamin.lesueur@live.com>
2024-12-24 15:39:37 +0100

    dirty

--- ADLX Wrapper/ADLX_Probe/Program.cs
+++ ADLX Wrapper/ADLX_Probe/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System.Runtime.InteropServices;
+using System.Text;
 
 namespace ADLX_Probe
 {
     class Program
     {
         public const string ADLX_Wrapper = @"ADLX_Wrapper.dll";
-        [DllImport(ADLX_Wrapper, CallingConvention = CallingConvention.Cdecl)] private static extern bool IntializeAdlx();
+        [DllImport(ADLX_Wrapper, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)] private static extern bool IntializeAdlx(StringBuilder dispName, int nameLength);
         [DllImport(ADLX_Wrapper, CallingConvention = CallingConvention.Cdecl)] private static extern bool InitializeAdlxWithIncompatibleDriver();
         [DllImport(ADLX_Wrapper, CallingConvention = CallingConvention.Cdecl)] public static extern bool CloseAdlx();
 
@@ -13,7 +14,8 @@ namespace ADLX_Probe
         {
             try
             {
-                if (IntializeAdlx())
+                StringBuilder displayName = new StringBuilder(256); // Assume display name won't exceed 255 characters
+                if (IntializeAdlx(displayName, displayName.Capacity))
                 {
                     CloseAdlx();
                     Environment.Exit(0); // Success
