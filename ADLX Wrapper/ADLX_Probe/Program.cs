e1c75aa67eb980d0a1f19646c6d64ad78d51f610
Lesueur Benjamin <benjamin.lesueur@live.com>
2025-03-01 14:45:03 +0100

    Improved ADLX_Probe, exclude ADLX 1.0 and below

--- ADLX Wrapper/ADLX_Probe/Program.cs
+++ ADLX Wrapper/ADLX_Probe/Program.cs
@@ -11,23 +11,33 @@ namespace ADLX_Probe
         [DllImport(ADLX_Wrapper, CallingConvention = CallingConvention.Cdecl)] private static extern bool InitializeAdlxWithIncompatibleDriver();
         [DllImport(ADLX_Wrapper, CallingConvention = CallingConvention.Cdecl)] public static extern bool CloseAdlx();
 
-        static StringBuilder displayName = new StringBuilder(256);
+        private static StringBuilder displayName = new StringBuilder(256);
+        private static Version ADLXVersion = new Version(1,0);
+
         static void Main()
         {
-            Timer UpdateTimer = new Timer(1000);
+            Timer UpdateTimer = new Timer(2000);
             UpdateTimer.Elapsed += UpdateTimer_Elapsed;
             UpdateTimer.Start();
 
-            // try
+            // initialize ADLX
             IntializeAdlx(displayName, displayName.Capacity);
 
-            while(true)
-                Thread.Sleep(1000);
+            // check version
+            ADLXVersion = new Version(displayName.ToString());
+            if (ADLXVersion.Major <= 1 && ADLXVersion.Minor <= 0)
+                Environment.Exit(0); // Failed, version is too old
+
+            while (true)
+                Thread.Sleep(500);
         }
 
         private static void UpdateTimer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
         {
-            Console.WriteLine(displayName.ToString());
+            if (sender is Timer timer)
+                timer.Stop();
+
+            Console.WriteLine(ADLXVersion.ToString());
             Environment.Exit(2); // Success
         }
     }
